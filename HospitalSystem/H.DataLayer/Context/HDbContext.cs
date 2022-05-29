b5b7b1e7c15566bd756e29a3ea080496cb49bfd4
behrooz vazirinasab <behrooz.bvn@hotmail.com>
2022-05-29 06:41:15 +0430

    0.001

---
+++ HospitalSystem/H.DataLayer/Context/HDbContext.cs
@@ -0,0 +1,23 @@
+﻿using H.DataMdel.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.DataLayer.Context
+{
+    public class HDbContext : DbContext
+    {
+        public HDbContext(DbContextOptions options)
+        : base(options)
+        {
+
+        }
+
+
+        public DbSet<DaroModel> Daros { get; set; }
+
+    }
+}
---
+++ HospitalSystem/H.IocConfig/AddCustomService.cs
@@ -0,0 +1,23 @@
+﻿using H.DataLayer.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.IocConfig
+{
+    public static class AddCustomService
+    {   
+        public static IServiceCollection AddCustomsServices(this IServiceCollection services)
+        {
+            services.AddDbContext<HDbContext>(options =>
+                {
+                    options.UseSqlServer();
+                });
+            return services;
+        }
+    }
+}
