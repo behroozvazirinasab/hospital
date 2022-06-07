2b1b4f9f49edc1c2707464408a8ffa975f9fea6e
behrooz vazirinasab <behrooz.bvn@hotmail.com>
2022-06-08 00:35:26 +0430

    0

--- HospitalSystem/TestProject1/ControllerTest/DaroControllerTest.cs
+++ HospitalSystem/TestProject1/ControllerTest/DaroControllerTest.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using H.DataMdel.ViewModel;
+using HospitalSystem.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace TestProject1.ControllerTest
 {
     public class DaroControllerTest
     {
+        [Fact]
+        public void Should_Return_Daro_Results()
+        {
+            //Arrange
+            var loggerMock = new Mock<IDaroService<DaroController>>();
+            var controller = new DaroController(loggerMock.Object);
+            //Act
+            var result = controller.CreateDaro();
+
+            //Assert            
+            result.ShouldNotBeNull();
+            result.Count().ShouldBeGreaterThan(1);
+
+
+        }
     }
 }
